ed68da1972168b0ea61b20c96f469f404bb485c4
Emmanuel Veyrat <emmanuel.veyrat@gmail.com>
2018-07-02 13:00:31 +0000

    Init

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# oauth2-arena
+# UWW Arena Provider for OAuth 2.0 Client
+
+This package provides Arena OAuth 2.0 support for the PHP League's [OAuth 2.0 Client](https://github.com/thephpleague/oauth2-client).
+
+## Installation
+
+To install, use composer:
+
+```
+composer require unitedworldwrestling/oauth2-arena
+```
+
+## Usage
+
+Usage is the same as The League's OAuth client, using `\Unitedworldwrestling\OAuth2\Client\Provider\Arena` as the provider.
+
+### Client Credentials Flow
+
+```php
+$provider = new Unitedworldwrestling\OAuth2\Client\Provider\Arena([
+    'clientId'          => '{arena-client-id}',
+    'clientSecret'      => '{arena-client-secret}',
+    'redirectUri'       => 'https://example.com/callback-url',
+    'host'              => 'https://api.instagram.com' // Optional, defaults to https://api.instagram.com
+]);
+
+
+// Try to get an access token (using the https://arena.uww.io/grants/api_key grant)
+$token = $provider->getAccessToken('https://arena.uww.io/grants/api_key');
+
+// Optional: Now you have a token you can look up a users profile data
+try {
+
+    // We got an access token, let's now get the user's details
+    $user = $provider->getResourceOwner($token);
+
+    // Use these details to create a new profile
+    printf('Hello %s!', $user->getName());
+
+} catch (Exception $e) {
+
+    // Failed to get user details
+    exit('Oh dear...');
+}
+
+// Use this to interact with an API on the users behalf
+echo $token->getToken();
+```
---
+++ src/Provider/Arena.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace Unitedworldwrestling\OAuth2\Client\Provider;
+
+use InvalidArgumentException;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use League\OAuth2\Client\Provider\AbstractProvider;
+use Psr\Http\Message\ResponseInterface;
+
+class Arena extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+    
+    /**
+     * @var string
+     */
+    private $urlAuthorize;
+
+    /**
+     * @var string
+     */
+    private $urlAccessToken;
+
+    /**
+     * @var string
+     */
+    private $urlResourceOwnerDetails;
+
+    /**
+     * @var string
+     */
+    private $accessTokenMethod;
+
+    /**
+     * @var string
+     */
+    private $accessTokenResourceOwnerId;
+
+    /**
+     * @var array|null
+     */
+    private $scopes = null;
+
+    /**
+     * @var string
+     */
+    private $scopeSeparator;
+
+    /**
+     * @var string
+     */
+    private $responseError = 'error';
+
+    /**
+     * @var string
+     */
+    private $responseCode;
+
+    /**
+     * @var string
+     */
+    private $responseResourceOwnerId = 'id';
+
+    /**
+     * @var string
+     */
+    protected $apiKey;
+
+    /**
+     * @param array $options
+     * @param array $collaborators
+     */
+    public function __construct(array $options = [], array $collaborators = [])
+    {
+        $this->assertRequiredOptions($options);
+
+        $possible   = $this->getConfigurableOptions();
+        $configured = array_intersect_key($options, array_flip($possible));
+
+        foreach ($configured as $key => $value) {
+            $this->$key = $value;
+        }
+
+        // Remove all options that are only used locally
+        $options = array_diff_key($options, $configured);
+
+        parent::__construct($options, $collaborators);
+    }
+
+    /**
+     * Returns all options that can be configured.
+     *
+     * @return array
+     */
+    protected function getConfigurableOptions()
+    {
+        return array_merge($this->getRequiredOptions(), [
+            'accessTokenMethod',
+            'accessTokenResourceOwnerId',
+            'scopeSeparator',
+            'responseError',
+            'responseCode',
+            'responseResourceOwnerId',
+            'scopes',
+        ]);
+    }
+
+    /**
+     * Returns all options that are required.
+     *
+     * @return array
+     */
+    protected function getRequiredOptions()
+    {
+        return [
+            'urlAuthorize',
+            'urlAccessToken',
+            'urlResourceOwnerDetails',
+        ];
+    }
+
+    /**
+     * Verifies that all required options have been passed.
+     *
+     * @param  array $options
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    private function assertRequiredOptions(array $options)
+    {
+        $missing = array_diff_key(array_flip($this->getRequiredOptions()), $options);
+
+        if (!empty($missing)) {
+            throw new InvalidArgumentException(
+                'Required options not defined: ' . implode(', ', array_keys($missing))
+            );
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return $this->urlAuthorize;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return $this->urlAccessToken;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return $this->urlResourceOwnerDetails;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getDefaultScopes()
+    {
+        return $this->scopes;
+    }
+
+    /**
+     * Requests an access token using a specified grant and option set.
+     *
+     * @param  mixed $grant
+     * @param  array $options
+     * @return AccessToken
+     */
+    // public function getAccessToken($grant, array $options = [])
+    // {
+    //     $grant = $this->verifyGrant($grant);
+
+    //     $params = [
+    //         'client_id'     => $this->clientId,
+    //         'client_secret' => $this->clientSecret,
+    //         'api_key'       => $this->apiKey,
+    //         'redirect_uri'  => $this->redirectUri,
+    //     ];
+
+    //     $params   = $grant->prepareRequestParameters($params, $options);
+    //     $request  = $this->getAccessTokenRequest($params);
+    //     $response = $this->getParsedResponse($request);
+    //     $prepared = $this->prepareAccessTokenResponse($response);
+    //     $token    = $this->createAccessToken($prepared, $grant);
+
+    //     return $token;
+    // }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getAccessTokenMethod()
+    {
+        return $this->accessTokenMethod ?: parent::getAccessTokenMethod();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getAccessTokenResourceOwnerId()
+    {
+        return $this->accessTokenResourceOwnerId ?: parent::getAccessTokenResourceOwnerId();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getScopeSeparator()
+    {
+        return $this->scopeSeparator ?: parent::getScopeSeparator();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if (!empty($data[$this->responseError])) {
+            $error = $data[$this->responseError];
+            if (!is_string($error)) {
+                $error = var_export($error, true);
+            }
+            $code  = $this->responseCode && !empty($data[$this->responseCode])? $data[$this->responseCode] : 0;
+            if (!is_int($code)) {
+                $code = intval($code);
+            }
+            throw new IdentityProviderException($error, $code, $data);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new ArenaResourceOwner($response, $this->responseResourceOwnerId);
+    }
+}
